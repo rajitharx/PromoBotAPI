74d9d9edb409664188a1fcad5365ef624f3c81d6
Rajitha Tharaka <rajithatharaka@gmail.com>
2019-01-31 09:58:36 +0800

    Add api BotController to the project

---
+++ PromoBotAPI/Controllers/BotController.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace PromoBotAPI.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+    }
+}
