5b30d060eb1ce63a0d91f89635588ad57f363af8
Rajitha Tharaka <rajithatharaka@gmail.com>
2019-02-14 14:08:37 +0800

    Testing reading log files

--- PromoBotAPI/Pages/About.cshtml.cs
+++ PromoBotAPI/Pages/About.cshtml.cs
@@ -14,15 +14,15 @@ namespace PromoBotAPI.Pages
 
         public void OnGet()
         {
-           // string line = string.Empty;
-           // FileStream fileStream = new FileStream("log.txt", FileMode.Open);
-           // using (StreamReader reader = new StreamReader(fileStream))
-           // {
-           //     line = reader.ReadToEnd();
-           // }
+            string line = string.Empty;
+            FileStream fileStream = new FileStream("log.txt", FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                line = reader.ReadToEnd();
+            }
 
-           //// Message = "Your application description page.";
-           // Message = line;
+            // Message = "Your application description page.";
+            Message = line;
         }
     }
 }
