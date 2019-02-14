f66e108e32eadd48eb233d5926e1491b6f0e5c66
Rajitha Tharaka <rajithatharaka@gmail.com>
2019-02-14 10:49:00 +0800

    add values

---
+++ PromoBot.Shared/Entity/User.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PromoBot.Shared.Entity
+{
+    using System;
+
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DOB { get; set; }
+    }
+}
